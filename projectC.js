51e838295bd9cede12ff7d91d50fd467bfaa93f7
cousincarrot <31780818+cousincarrot@users.noreply.github.com>
2017-11-08 00:04:31 -0600

    got pointer x,y displaying
    well, with the point function commented out, the x,y location display works. Not sure about ctx, need to read up on it.

--- projectC.js
+++ projectC.js
@@ -1,1 +1,19 @@
+//TO DO
 
+function animation(event){
+    console.log("inside animation");
+
+    var x = event.clientX;
+    var y = event.clientY;
+    var coords = "X coords: " + x + ", Y coords: " + y;
+
+
+
+    document.getElementById("box").innerHTML = coords;
+
+    function point(x, y, box){
+      console.log("inside point");
+      ctx.fillRect(10,10,1,1);
+    }
+    point();
+};
